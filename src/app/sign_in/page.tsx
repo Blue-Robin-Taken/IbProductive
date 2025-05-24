538ae324ddef03630910d5ee930a4d5bd66985fa
whYXZee <email@email.com>
2025-05-24 15:09:36 -0500

    add sign in page

---
+++ src/app/sign_in/page.tsx
@@ -0,0 +1,16 @@
+export default function SignIn() {
+    return (
+        <div className="inline-flex border-indigo-500">
+            <div> 
+                {/* Username */}
+            </div>
+            <div>
+                {/* password */}
+            </div>
+            <div>
+                {/* button */}
+                <button>Sign in</button>
+            </div>
+        </div>
+    );
+}
