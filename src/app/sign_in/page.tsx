6514b9e530e784fb3ac6923896cfe0c060bfb811
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-05-24 15:11:30 -0500

    form

--- src/app/sign_in/page.tsx
+++ src/app/sign_in/page.tsx
@@ -1,16 +1,13 @@
 export default function SignIn() {
     return (
         <div className="inline-flex border-indigo-500">
-            <div> 
-                {/* Username */}
-            </div>
-            <div>
-                {/* password */}
-            </div>
-            <div>
-                {/* button */}
+            <form>
+                <label htmlFor="fname">First name:</label>
+                <input type="text" id="fname" name="fname" />
+                <label htmlFor="lname">Last name:</label>
+                <input type="text" id="lname" name="lname" />
                 <button>Sign in</button>
-            </div>
+            </form>
         </div>
     );
 }
