579010f9b78aaaafd6c3f8cec8dbd0b15171e776
Julian Thomas <julianjohnt@gmail.com>
2025-06-12 20:33:47 -0500

    created form handler that creates a post request to the API

--- src/app/api/auth/signUp/route.ts
+++ src/app/api/auth/signUp/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 /* Implementation to authenticate the user*/
 export async function POST(request: Request) {
-    function hasUpperCase(str: String) {
-        return str !== str.toLowerCase();
-    }
+  function hasUpperCase(str: String) {
+    return str !== str.toLowerCase();
+  }
 
-    console.log(request.formData());
+  console.log(request.formData());
 }
--- src/app/sign_up/page.tsx
+++ src/app/sign_up/page.tsx
@@ -1,11 +1,30 @@
+"use client";
+/* Form Validation should be handled on the API level for 
+security (but this is the client side part) */
+import { FormEvent } from "react";
+
+function submitForm(e: FormEvent): void {
+  e.preventDefault();
+  const username = (e.target as HTMLFormElement).username.value;
+  const email = (e.target as HTMLFormElement).email.value;
+  const password = (e.target as HTMLFormElement).password.value;
+  fetch("/api/auth/signUp", {
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      username: username,
+      password: password,
+    }),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+  });
+}
+
 export default function SignIn() {
   return (
     <div className="flex justify-center">
-      <form
-        className="grid grid-rows-3 gap-5"
-        method="POST"
-        action="/api/create_account"
-      >
+      <form className="grid grid-rows-3 gap-5" onSubmit={submitForm}>
         <label className="text-center" htmlFor="email">
           Email:
         </label>
