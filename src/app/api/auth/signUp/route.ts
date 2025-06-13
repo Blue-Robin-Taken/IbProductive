cbbec401f22b393d49a6571751fd678af70e00f8
Julian Thomas <julianjohnt@gmail.com>
2025-06-12 21:52:02 -0500

    fixed prisma client

--- src/app/api/auth/signUp/route.ts
+++ src/app/api/auth/signUp/route.ts
@@ -2,12 +2,13 @@ import { NextResponse } from "next/server";
 
 import { Resend } from "resend";
 import { config } from "dotenv";
+import { randomUUID } from "crypto";
+
+import { createAccountVerificationToken } from "@/db/index";
 
 config();
 
 const resend = new Resend(process.env["EMAIL_API_KEY"]);
-
-console.log(resend);
 
 interface responseType {
   username: string;
@@ -24,15 +25,18 @@ export async function POST(request: Request) {
   const res: responseType = await request.json();
 
   /* Save the verification token for the account in the prisma database */
-  
 
   /* Send Email*/
+  const token = randomUUID(); // or custom string
+
   await resend.emails.send({
     from: "IBProductive <onboarding@resend.dev>",
     to: [res.email],
     subject: "hello world",
     html: "<p>it works!</p>",
   });
+
+  createAccountVerificationToken(token, res.email, "", ""); // Todo, implement pass hashing
 
   return new NextResponse(JSON.stringify({ error: 0 }));
 }
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,33 @@
-import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { randomUUID } from "crypto";
+import { addMinutes } from "date-fns";
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
 
 async function createUser() {
   // prisma.
+}
+
+export async function createAccountVerificationToken(
+  token: string,
+  email: string,
+  username: string,
+  password: string
+) {
+  /* Used specifically to create
+    a token in the prisma database so that the 
+    account can be created after the user's verfification
+    email is accepted. This is the token that will
+    be referenced in that process. */
+  await prisma.verificationToken.create({
+    data: {
+      email,
+      token, // TODO: Implement password hashing here
+      expiresAt: addMinutes(new Date(), 15), // expires in 15 mins
+    },
+  });
+
+  return token;
 }
