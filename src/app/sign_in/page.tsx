9f3532517346399c511ab481c9c19f645c9c1bc0
whYXZee <email@email.com>
2025-07-17 21:37:02 -0500

    tasks are only dispalyed for the user

--- src/app/sign_in/page.tsx
+++ src/app/sign_in/page.tsx
@@ -1,55 +1,61 @@
-'use client'; // Form validation should be done both client side and server side!!!
+"use client"; // Form validation should be done both client side and server side!!!
 // Do not store anything sensitive here!!
-import { FormEvent } from 'react';
+import { FormEvent, useState } from "react";
 
-async function submitForm(e: FormEvent) {
+export default function SignIn() {
+  const [formValid, setFormValid] = useState("");
+
+  async function submitForm(e: FormEvent) {
     e.preventDefault();
 
     const username = (e.target as HTMLFormElement).username.value;
     const password = (e.target as HTMLFormElement).passkey.value;
 
-    const req = await fetch('/api/auth/signIn', {
-        method: 'POST',
-        body: JSON.stringify({
-            username: username,
-            password: password,
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
+    const req = await fetch("/api/auth/signIn", {
+      method: "POST",
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
-}
 
-export default function SignIn() {
-    return (
-        <div className="flex justify-center ">
-            <form className="grid grid-rows-3 gap-5" onSubmit={submitForm}>
-                <label className="text-center" htmlFor="username">
-                    Username:
-                </label>
-                <input
-                    className="text-black"
-                    type="text"
-                    id="username"
-                    name="username"
-                />
-                <label className="text-center" htmlFor="passkey">
-                    Password:
-                </label>
-                <input
-                    className="text-black"
-                    type="password"
-                    id="passkey"
-                    name="passkey"
-                />
-                <input
-                    className="bg-lime-300 text-black"
-                    type="submit"
-                    value="Submit"
-                />
-            </form>
-            {/* Handle response */}
-            {}
-        </div>
-    );
+    setFormValid(await req.text());
+  }
+
+  return (
+    <div className="flex justify-center ">
+      <form className="grid grid-rows-3 gap-5" onSubmit={submitForm}>
+        <label className="text-center" htmlFor="username">
+          Username:
+        </label>
+        <input
+          className="text-black"
+          type="text"
+          id="username"
+          name="username"
+        />
+        <label className="text-center" htmlFor="passkey">
+          Password:
+        </label>
+        <input
+          className="text-black"
+          type="password"
+          id="passkey"
+          name="passkey"
+        />
+        <input
+          className="bg-lime-300 text-black"
+          type="submit"
+          value="Submit"
+        />
+      </form>
+
+      {/* Handle response */}
+      {formValid && <p>{formValid}</p>}
+      {/* if true, then do other end of ampersand */}
+    </div>
+  );
 }
