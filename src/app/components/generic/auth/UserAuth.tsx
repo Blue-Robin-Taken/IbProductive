bce26040aa5773a14b1bfe6e0b0cf75606a78153
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-08-04 21:45:36 -0500

    useeffect updates on navigation change

--- src/app/components/generic/auth/UserAuth.tsx
+++ src/app/components/generic/auth/UserAuth.tsx
@@ -1,22 +1,25 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { useEffect, useState } from "react";
-
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 export default function UserLayout() {
   const [loggedIn, setLoggedIn] = useState(false);
 
+  const location = usePathname();
+
   useEffect(() => {
-    fetch("/api/auth/user?")
+    fetch('/api/auth/user?')
       .then((res) => {
         return res.text();
       })
       .then((text) => {
-        if (text == "token") {
+        if (text == 'token') {
           setLoggedIn(true);
         }
       });
-  }, []);
+    console.log('hi');
+  }, [location]);
 
   return loggedIn ? (
     <Link href="/settings">Settings</Link>
